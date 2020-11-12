4fe6f06f449d3fbc3f81765b300143b9d757515d
Bridget Shannon <bridgetrshannon@gmail.com>
2020-11-11 20:24:56 -0500

    hooked in mdBootstrap framework, added a placeholder navbar

---
+++ client/src/components/About.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <div>
+      {/* <MDBContainer> */}
+        <p>About goes here</p>
+      {/* </MDBContainer> */}
+    </div>
+  );
+};
+
+export default About;
