e5bfd277e65ba53e3bd8fb79ee83b8158db55b59
leanndemetro <leanndemetro@gmail.com>
2020-11-13 14:20:47 -0500

    login authentication fully funtional and values are sent to database

---
+++ config/keys.js
@@ -0,0 +1,4 @@
+module.exports = {
+    mongoURI: "mongodb://localhost/assist",
+    secretOrKey: "secret"
+};
--- server.js
+++ server.js
@@ -2,11 +2,40 @@ const express = require("express");
 const path = require("path");
 const PORT = process.env.PORT || 3001;
 const app = express();
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const db = require("./config/keys").mongoURI;
+const passport = require("passport");
+const users = require("./routes/api/users");
+
+// Bodyparser middleware
+app.use(
+  bodyParser.urlencoded({
+    extended: false
+  })
+);
+app.use(bodyParser.json());
+
+// Connect to MongoDB
+mongoose
+  .connect(
+    db,
+    { useNewUrlParser: true }
+  )
+  .then(() => console.log("MongoDB successfully connected"))
+  .catch(err => console.log(err));
 
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
+
+// Passport middleware
+app.use(passport.initialize());
+// Passport config
+require("./config/passport")(passport);
+// Routes
+app.use("/api/users", users);
 
 // Send every request to the React app
 // Define any API routes before this runs
