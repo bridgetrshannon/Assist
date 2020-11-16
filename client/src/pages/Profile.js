13a577f981941161648ff4e4245fa3a1a1e87cf0
Bridget Shannon <bridgetrshannon@gmail.com>
2020-11-15 23:22:24 -0500

    separated components for profile page and imported them, added crud buttons and placeholder images

---
+++ client/src/pages/Profile.js
@@ -0,0 +1,14 @@
+import React from "react";
+import MyPosts from "../components/MyPosts";
+import SavedOpportunities from "../components/SavedOpportunities";
+
+const Profile = () => {
+  return (
+    <div>
+      <SavedOpportunities />
+      <MyPosts />
+    </div>
+  );
+};
+
+export default Profile;
