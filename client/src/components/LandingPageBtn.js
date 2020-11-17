8375cdfe9a52c681b189f1dbebb69b96319b23d2
leanndemetro <leanndemetro@gmail.com>
2020-11-17 13:45:37 -0500

    buttons linked to correct routes (except dropdown)

--- client/src/components/LandingPageBtn.js
+++ client/src/components/LandingPageBtn.js
@@ -4,11 +4,12 @@ import { MDBBtn } from "mdbreact";
 const LandingPageBtn = () => {
   return (
     <div>
-      <MDBBtn className="rounded p-2" color="white" outline type="submit">
-        Log In
+      <MDBBtn className="rounded p-2" color="white" outline type="submit" href="/login">
+        Log In 
       </MDBBtn>
       <span className="align-middle p-1">or</span>
-      <MDBBtn className="rounded p-2" color="white" outline type="submit">
+      <MDBBtn className="rounded p-2" color="white" outline type="submit" href="/signup
+      ">
         Sign Up
       </MDBBtn>
     </div>
