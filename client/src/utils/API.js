fb8690daabbc1a64f653a4024cbac2c2eab2f2df
leanndemetro <leanndemetro@gmail.com>
2020-11-20 11:30:03 -0500

    cors error fixed

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -3,7 +3,7 @@ import axios from "axios";
 export default {
   // Gets all opportunites from API if no state is selected
   getAllOpportunity: function() {
-    return axios.get("https://api.data.charitynavigator.org/v2/Organizations?app_id=1cd02618&app_key=d031dafe2f636e0d080019ce0eb8be5a&search=" );
+    return axios.get(`${'https://cors-anywhere.herokuapp.com/'}https://api.data.charitynavigator.org/v2/Organizations?app_id=1cd02618&app_key=d031dafe2f636e0d080019ce0eb8be5a&search=` );
   }
 //   // Gets all opportunites from API by user's chosen state
 //   getStateOpportunity: function(userState) {
