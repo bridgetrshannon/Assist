c7cd6f5d416c89dd31fd2db3cc6ed766df9c4321
Bridget Shannon <bridgetrshannon@gmail.com>
2020-11-20 10:46:17 -0500

    added field for organization in create form, haven't added it into searchCard.js though

--- client/src/components/SearchCard.js
+++ client/src/components/SearchCard.js
@@ -12,96 +12,87 @@ import {
 import API from "../utils/API";
 import { List, ListItem } from "../components/List/index";
 
-
 function Opportunity() {
   // Setting our component's initial state
-  const [opportunity, setOpportunity] = useState([])
-  const [formObject, setFormObject] = useState({})
+  const [opportunity, setOpportunity] = useState([]);
+  const [formObject, setFormObject] = useState({});
 
   // Load all opportunities and store them with loadBooks
   useEffect(() => {
-    loadAll()
-  }, [])
+    loadAll();
+  }, []);
 
-  // Loads all opportunities and sets them to 
+  // Loads all opportunities and sets them to
   function loadAll() {
     API.getAllOpportunity()
-      .then(res =>  {
-        setOpportunity(res.data)
+      .then((res) => {
+        setOpportunity(res.data);
         console.log(res);
-      }     
-      )
-      .catch(err => console.log(err));
-  };
-
+      })
+      .catch((err) => console.log(err));
+  }
 
   return (
     <div>
-    
-    {
-      opportunity.length ? (
+      {opportunity.length ? (
         <List>
-          {opportunity.map(opportunity => {
+          {opportunity.map((opportunity) => {
             return (
               <ListItem key={opportunity.ein}>
-              <MDBContainer>
-                <MDBCard className="w-100 mb-4 m">
-                  <MDBCardBody>
-                    <MDBRow>
-                      <MDBCol>
-                        <img
-                          alt="searchImg"
-                          src="https://post.greatist.com/wp-content/uploads/sites/3/2020/02/322868_1100-1100x628.jpg"
-                          width="160px"
-                        />
-                      </MDBCol>
-                      <MDBCol>
-                        <MDBCardTitle>{opportunity.charityName}</MDBCardTitle>
-                        <MDBCardText>
-                          {opportunity.irsClassification.classification}
-                        </MDBCardText>
-                      </MDBCol>
-                      <MDBCol>
-                        <MDBCardTitle>Located at:</MDBCardTitle>
-                        <MDBCardText>
-                          {opportunity.mailingAddress.streetAddress1} <br></br>
-                          {opportunity.mailingAddress.city + ", "}
-                          {opportunity.mailingAddress.stateOrProvince}<br></br>
-                          {opportunity.mailingAddress.postalCode}
-
-                        </MDBCardText>
-                      </MDBCol>
-                    </MDBRow>
-                    <MDBRow>
-                      <MDBCol>
-                      </MDBCol>
-                      <MDBCol>
-                        <MDBBtn
-                          color="deep-purple"
-                          outline
-                          type="submit"
-                          className="text-center mt-4 mb-1 pl-5 pr-5"
-                        >
-                          Save this opportunity
-            </MDBBtn>
-                      </MDBCol>
-                    </MDBRow>
-                  </MDBCardBody>
-                </MDBCard>
+                <MDBContainer>
+                  <MDBCard className="w-100 mb-4 m">
+                    <MDBCardBody>
+                      <MDBRow>
+                        <MDBCol>
+                          <img
+                            alt="searchImg"
+                            src="https://post.greatist.com/wp-content/uploads/sites/3/2020/02/322868_1100-1100x628.jpg"
+                            width="160px"
+                          />
+                        </MDBCol>
+                        <MDBCol>
+                          <MDBCardTitle>{opportunity.charityName}</MDBCardTitle>
+                          <MDBCardText>
+                            {opportunity.irsClassification.classification}
+                          </MDBCardText>
+                        </MDBCol>
+                        <MDBCol>
+                          <MDBCardTitle>Located at:</MDBCardTitle>
+                          <MDBCardText>
+                            {opportunity.mailingAddress.streetAddress1}{" "}
+                            <br></br>
+                            {opportunity.mailingAddress.city + ", "}
+                            {opportunity.mailingAddress.stateOrProvince}
+                            <br></br>
+                            {opportunity.mailingAddress.postalCode}
+                          </MDBCardText>
+                        </MDBCol>
+                      </MDBRow>
+                      <MDBRow>
+                        <MDBCol></MDBCol>
+                        <MDBCol>
+                          <MDBBtn
+                            color="deep-purple"
+                            outline
+                            type="submit"
+                            className="text-center mt-4 mb-1 pl-5 pr-5"
+                          >
+                            Save this opportunity
+                          </MDBBtn>
+                        </MDBCol>
+                      </MDBRow>
+                    </MDBCardBody>
+                  </MDBCard>
                 </MDBContainer>
               </ListItem>
             );
           })}
         </List>
       ) : (
-          <h3>No Results to Display</h3>
-        )
-    }
+        <h3>No Results to Display</h3>
+      )}
     </div>
   );
 }
-
-
-
 
 export default Opportunity;
