bb4d84226b8e6e71497f877f55c7e43e57d892bc
leanndemetro <leanndemetro@gmail.com>
2020-11-22 22:15:32 -0500

    filters by search when a user selects one from dropdown

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -11,7 +11,7 @@ export default {
     // Gets all opportunites from API by user's chosen state
     getStateOpportunity: function(capturedState) {
       return axios.get(
-        `${"https://cors-anywhere.herokuapp.com/"} + https://api.data.charitynavigator.org/v2/Organizations?app_id=1cd02618&app_key=d031dafe2f636e0d080019ce0eb8be5a&state=` + capturedState );
+        `${"https://cors-anywhere.herokuapp.com/"}https://api.data.charitynavigator.org/v2/Organizations?app_id=1cd02618&app_key=d031dafe2f636e0d080019ce0eb8be5a&state=` + capturedState );
     
       },
   //   // Deletes the book with the given id
