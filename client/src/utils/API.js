a1185cb91428aeff8e1bc387236358ccc2bca911
leanndemetro <leanndemetro@gmail.com>
2020-11-22 21:52:14 -0500

    unworking code DO NOT MERGE

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -1,4 +1,5 @@
 import axios from "axios";
+import capturedState from "../components/SearchCard"
 
 export default {
   // Gets all opportunites from API if no state is selected
@@ -7,10 +8,12 @@ export default {
       `${"https://cors-anywhere.herokuapp.com/"}https://api.data.charitynavigator.org/v2/Organizations?app_id=1cd02618&app_key=d031dafe2f636e0d080019ce0eb8be5a&search=`
     );
   },
-  //   // Gets all opportunites from API by user's chosen state
-  //   getStateOpportunity: function(userState) {
-  //     return axios.get("https://api.data.charitynavigator.org/v2/Organizations?app_id=1cd02618&app_key=d031dafe2f636e0d080019ce0eb8be5a&state=" + userState );
-  //   },
+    // Gets all opportunites from API by user's chosen state
+    getStateOpportunity: function(capturedState) {
+      return axios.get(
+        `${"https://cors-anywhere.herokuapp.com/"} + https://api.data.charitynavigator.org/v2/Organizations?app_id=1cd02618&app_key=d031dafe2f636e0d080019ce0eb8be5a&state=` + capturedState );
+    
+      },
   //   // Deletes the book with the given id
   //   deleteOpportunity: function(ein) {
   //     return axios.delete("/api/opportunity/" + id);
